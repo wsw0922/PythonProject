2ef9639e98c7737b38bb35eb2e30e2e6e499c065
JaeSeok <wnwotjr8@naver.com>
2023-05-25 22:28:26 +0900

    20230525

--- code_madeByjjs/cmpRain.py
+++ code_madeByjjs/cmpRain.py
@@ -21,5 +21,31 @@ sns.set(style="darkgrid",font=font)
 bar1 = plt.bar(distName, minHeight, width=0.5, label='최저고도',align='edge')
 bar2 = plt.bar(distName, majorHeight, width=0.3, label='최빈값',align='center')
 plt.xticks(rotation=30)
+
+plt.title('서울시 주요 자치구 해발고도')
+plt.xlabel('자치구명')
+plt.ylabel('단위(m)')
 plt.legend()
+plt.show()
+
+# 월 강수량 꺽은선 그래프
+## 데이터 입력
+rain = pd.read_csv("./data/서울 지역구별 월강수량.csv",index_col='구청명')
+month = np.array([f'{i}월' for i in range(1,13)])
+colors = ['red', 'orange', 'green', 'blue', 'purple']
+## 그래프
+plt.figure(figsize=(10,5))
+sns.set(style="darkgrid",font=font)
+colSelect = 0
+for dist in distName:
+    rainData = np.array(rain.loc[dist])
+    plt.plot(month, rainData, color=colors[colSelect], label=dist)
+    colSelect += 1
+    
+plt.title('서울시 주요 자치구 월 강수량')
+plt.xlabel('2022년')
+plt.ylabel('단위(mm)')
+
+plt.legend()
+
 plt.show()
