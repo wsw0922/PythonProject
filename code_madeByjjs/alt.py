03522c94b126a0fdac7ba4b3ff2b5a28af5f802d
JaeSeok <wnwotjr8@naver.com>
2023-05-25 16:19:41 +0900

    "피해가 큰 5개 지역구의 고도 그래프"

--- code_madeByjjs/alt.py
+++ code_madeByjjs/alt.py
@@ -1,5 +1,4 @@
-# 서울시 고도 데이터 분석
-
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import font_manager
@@ -7,16 +6,20 @@ font_path = r"C:\Windows\Fonts\H2GTRE.TTF"
 font = font_manager.FontProperties(fname=font_path).get_name()
 import seaborn as sns
 
-#데이터 입력
-alti = pd.read_csv("./data/altitudeOfSeoul.csv")
-alti = alti.iloc[:,1:]
+# 비교할 자치구들
+distName = np.array(['영등포구','동작구','관악구','서초구','구로구'])
 
-minAlti = alti[['지역','최저고도']]
+# 고도데이터 그래프
+## 데이터 입력
+alti = pd.read_csv("./data/altitudeOfSeoul.csv",index_col='지역')
+minHeight = np.array(alti['최저고도'][distName]) # 최저 고도
+majorHeight = np.array(alti['최빈값'][distName]) # 최빈값
 
-#그래프
-order = alti.index #x축 내용
-plt.figure(figsize=(25,5))
+## 그래프
+plt.figure(figsize=(5,5))
 sns.set(style="darkgrid",font=font)
-ax = sns.barplot(x='지역',y='최저고도',data=minAlti)
-plt.xticks(rotation=45)
+bar1 = plt.bar(distName, minHeight, width=0.5, label='최저고도',align='edge')
+bar2 = plt.bar(distName, majorHeight, width=0.3, label='최빈값',align='center')
+plt.xticks(rotation=30)
+plt.legend()
 plt.show()
